689c7d2937663d7f13997578c260d6d56248c182
Yurii Lichutin <yuriilichutin@hotmail.com>
2015-10-20 23:24:25 +0300

    multiplayers

--- public_html/js/app/game/core.js
+++ public_html/js/app/game/core.js
@@ -1,51 +1,65 @@
 define(['jquery'], function ($) {
+    'use strict';
+
     var players = [];
 
-    /*
-     * Game core
-     * @type type
-     */
+    var setPlayer = function (player)
+    {
+        players.push(player);
+        setControl(player);
+
+        var id = players.indexOf(player);
+        player.id = id;
+
+        drawPlayer(player);
+
+        return players.indexOf(player);
+    };
+
+    var drawPlayer = function (player)
+    {
+        $('body').append('<div class="gamer" id="player-' + player.id + '">player ' + player.id + '</div>');
+    };
+
+    var setControl = function (player)
+    {
+        var apply = function () {
+            $('#player-' + player.id).css('top', position.y);
+            $('#player-' + player.id).css('left', position.x);
+        };
+
+        var position = {x: 0, y: 0};
+        var control = {
+            moveUp: function () {
+                position.y -= 50;
+                apply();
+            },
+            moveDown: function () {
+                position.y += 50;
+                apply();
+
+            },
+            moveLeft: function () {
+                position.x -= 50;
+                apply();
+
+            },
+            moveRight: function () {
+                position.x += 50;
+                apply();
+            }
+        };
+
+        player.setControl(control);
+
+    };
+
     var core = {
-        /*
-         * Return an player's id
-         */
-        setPlayer: function (player) {
-            players.push(player);
-            player.setControl(control);
-            return players.indexOf(player);
-        },
+        setPlayer: setPlayer,
         startGame: function () {
             return 'game started!';
         }
     };
-
-    var position = {x: 0, y: 0};
-
-    var apply = function () {
-        $('#player-0').css('top', position.y);
-        $('#player-0').css('left', position.x);
-    };
-
-    var control = {
-        moveUp: function () {
-            position.y -= 50;
-            apply();
-        },
-        moveDown: function () {
-            position.y += 50;
-            apply();
-
-        },
-        moveLeft: function () {
-            position.x -= 50;
-            apply();
-
-        },
-        moveRight: function () {
-            position.x += 50;
-            apply();
-        }
-    }
 
     return core;
 });
