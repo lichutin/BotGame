88cad1b3caae3b86bc5503034043b4b72f5aaf78
artik <artik.alpha@gmail.com>
2015-10-21 13:34:02 +0300

    add: battlefield, change: addPlayer

--- public_html/js/app/app.js
+++ public_html/js/app/app.js
@@ -3,32 +3,61 @@ define('app', ['jquery', 'game/core'], function ($, core) {
 
     var app = {};
 
-    var newPlayer = {name: 'Yurii', setControl: function (control) {
-            $(document).on('keydown', function (e) {
-                var event = window.event ? window.event : e;
-                switch (event.keyCode)
-                {
-                    case 37:
-                        control.moveLeft();
-                        break;
-                    case 38:
-                        control.moveUp();
-                        break;
-                    case 39:
-                        control.moveRight();
-                        break;
-                    case 40:
-                        control.moveDown();
-                        break;
-                    default:
-                        break;
-                }
-            });
-        }};
+    var AddPlayer = function (info) {
+        var Player = {name: info.name, setControl: function (control) {
+                $(document).on('keydown', function (e) {
+                    var event = window.event ? window.event : e;
+                    console.log("event.keyCode = " + event.keyCode);
+                    switch (event.keyCode)
+                    {
+                        case info.control.fire:
+                            control.Fire();
+                            break;
+                        case info.control.left:
+                            control.moveLeft();
+                            break;
+                        case info.control.up:
+                            control.moveUp();
+                            break;
+                        case info.control.right:
+                            control.moveRight();
+                            break;
+                        case info.control.down:
+                            control.moveDown();
+                            break;
+                        default:
+                            break;
+                    }
+                });
+            }};
+        var id = core.setPlayer(Player);
+        console.log('new player', Player.name, 'id', id);
+    };
 
-    var id = core.setPlayer(newPlayer);
 
-    console.log('new player', newPlayer.name, 'id', id);
+    var yurii = {
+        name: "Yurii",
+        control: {
+            fire: 96,
+            up: 38,
+            left: 37,
+            down: 40,
+            right: 39
+        }
+    };
+    var artik = {
+        name: "Artik",
+        control: {
+            fire: 32,
+            up: 87,
+            left: 65,
+            down: 83,
+            right: 68
+        }
+    };
+
+    AddPlayer(yurii);
+    AddPlayer(artik);
 
     return app;
 });
--- public_html/js/app/game/core.js
+++ public_html/js/app/game/core.js
@@ -1,6 +1,7 @@
 define(['jquery'], function ($) {
     'use strict';
 
+    var colors = ["#f00", "#ff0"];
     var players = [];
 
     var setPlayer = function (player)
@@ -18,18 +19,36 @@ define(['jquery'], function ($) {
 
     var drawPlayer = function (player)
     {
-        $('body').append('<div class="gamer" id="player-' + player.id + '">player ' + player.id + '</div>');
+        var gamer = $("<div />").addClass("gamer").attr("id", "player-"+ player.id).text("Player #"+ player.id).css("background", colors[player.id]);
+        $('.battlefield').append(gamer);
     };
 
     var setControl = function (player)
     {
         var apply = function () {
-            $('#player-' + player.id).css('top', position.y);
-            $('#player-' + player.id).css('left', position.x);
+            if (position.x < 500 && position.y < 500 && position.x >= 0 && position.y >= 0) {
+                $('#player-' + player.id).css('top', position.y);
+                $('#player-' + player.id).css('left', position.x);
+            }
+            else if (position.x >= 500) {
+                position.x = 450;
+            }
+            else if (position.y >= 500) {
+                position.y = 450;
+            }
+            else if (position.x < 0){
+                position.x = 0;
+            }
+            else if (position.y < 0){
+                position.y = 0;
+            }
         };
 
         var position = {x: 0, y: 0};
         var control = {
+            Fire: function(){
+              console.log("fire!"); 
+            },
             moveUp: function () {
                 position.y -= 50;
                 apply();
