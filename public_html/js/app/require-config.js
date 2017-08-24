50ee8f2e237200a25986fdf3dfd893735b673885
Artik <AMangasaryan@luxoft.com>
2017-08-24 13:10:33 +0300

    НЕ СМОТРИ КОД.

--- public_html/js/app/require-config.js
+++ public_html/js/app/require-config.js
@@ -2,6 +2,7 @@ require.config({
     baseUrl: "js/app",
     paths: {
         'jquery': '../libs/jquery',
+        '_':'../libs/lodash.core',
         'botYurii': 'bots/botYurii',
         'botArtik': 'bots/botArtik'
     },
