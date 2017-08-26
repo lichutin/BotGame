0aaf736b3daa37bd56fa39e9ba8b76f118b4b343
Artik Man <artik.alpha@gmail.com>
2017-08-26 19:01:26 +0300

    UPD: botArtik

--- public_html/js/app/bots/botArtik.js
+++ public_html/js/app/bots/botArtik.js
@@ -1,292 +1,114 @@
-define(['game/core', '_'], function (core, _) {
-    // var me,
-    //     enemy,
-    //     bullets = [],
-    //     canIStayHere = true;
+define(['game/core', '_'], function(core, _) {
 
-    // var nextPositions = {
-    //     top: {
-    //         x: 0,
-    //         y: 0,
-    //         good: false
-    //     },
-    //     right: {
-    //         x: 0,
-    //         y: 0,
-    //         good: false
-    //     },
-    //     left: {
-    //         x: 0,
-    //         y: 0,
-    //         good: false
-    //     },
-    //     bottom: {
-    //         x: 0,
-    //         y: 0,
-    //         good: false
-    //     },
-    // };
+	var _step = 0,
+		_info,
+		_self,
+		_enemy,
+		_direction = 1;
 
-    var hitTest = function (x1, y1, size1, x2, y2, size2) {
-        return ((x1 + size1 >= x2) && (x1 <= x2 + size2) && (y1 + size1 >= y2) && (y1 <= y2 + size2));
-    }
+	var hitTest = function(x1, y1, size1, x2, y2, size2) {
+		return ((x1 + size1 >= x2) && (x1 <= x2 + size2) && (y1 + size1 >= y2) && (y1 <= y2 + size2));
+	};
 
-    var damageForStep = function (objects, _me, nextStep) {
-        me = _me;
-        enemy = _.find(objects, function (o) {
-            return o.type === 1 && o.id !== me.id;
-        });
-        if (nextStep.x < 0 || nextStep.x > 450 || nextStep.y < 0 || nextStep.y > 450) {
-            return 1000;
-        }
-        if (nextStep.x == enemy.position.xCur && nextStep.x == enemy.position.yCur) {
-            return 1000;
-        }
-        var enemyShots = _.filter(objects, function (o) {
-            return o.type == 2 && o.pid === enemy.id;
-        });
+	var canIMove = function(nextStep) {
+		if (nextStep.x === _enemy.position.xCur && nextStep.x === _enemy.position.yCur) {
+			return false;
+		}
+		return !(nextStep.x < 0 || nextStep.x > 450 || nextStep.y < 0 || nextStep.y > 450)
+	};
 
-        return _.filter(enemyShots, function (shot) {
-            return (hitTest(nextStep.x, nextStep.y, me.size.width, shot.position.xCur, shot.position.yCur, shot.size.height))
-        }).length;
-    }
+	var damageForStep = function(nextStep) {
+		var enemyShots = _.filter(_info, function(o) {
+			return o.type === 2 && o.pid === _enemy.id;
+		});
+		enemyShots.push(_enemy);
 
-    var analyzePosition = function (objects, _me) {
-        me = _me;
-        enemy = _.find(objects, function (o) {
-            return o.type === 1 && o.id !== me.id;
-        });
-        var enemyShots = _.filter(objects, function (o) {
-            return o.type == 2 && o.pid === enemy.id;
-        });
-        bullets = enemyShots;
-        enemyShots.push({
-            id: enemy.id,
-            position: enemy.position,
-            size: enemy.size
-        })
-        // console.log(myId, enemyId)
-        // console.log(objects)
+		return _.filter(enemyShots, function(shot) {
+			return (hitTest(nextStep.x, nextStep.y, _self.size.width, shot.position.xCur, shot.position.yCur, shot.size.height))
+		}).length;
+	};
 
-        var distance = 50;
-        var hitArea = me.size.width;
+	var sortingPositions = function(xCur, yCur) {
+		var out = [];
+		var positions = [
+			{
+				x: xCur,
+				y: yCur - 50,
+				move: core.actionTypes.moveUp
+			},
+			{
+				x: xCur + 50,
+				y: yCur,
+				move: core.actionTypes.moveRight
+			},
+			{
+				x: xCur,
+				y: yCur + 50,
+				move: core.actionTypes.moveDown
+			},
+			{
+				x: xCur - 50,
+				y: yCur,
+				move: core.actionTypes.moveLeft
+			}
+		];
+		var sorting = {};
+		sorting[core.actionTypes.moveUp] = [core.actionTypes.moveUp, core.actionTypes.moveLeft, core.actionTypes.moveRight, core.actionTypes.moveDown];
+		sorting[core.actionTypes.moveDown] = [core.actionTypes.moveDown, core.actionTypes.moveRight, core.actionTypes.moveLeft, core.actionTypes.moveUp];
+		sorting[core.actionTypes.moveLeft] = [core.actionTypes.moveLeft, core.actionTypes.moveDown, core.actionTypes.moveUp, core.actionTypes.moveRight];
+		sorting[core.actionTypes.moveRight] = [core.actionTypes.moveRight, core.actionTypes.moveUp, core.actionTypes.moveDown, core.actionTypes.moveLeft];
 
-        nextPositions.top.x = me.position.xCur;
-        nextPositions.top.y = me.position.yCur - distance;
-        nextPositions.top.good = true;
+		var selectedStrategy = sorting[_direction];
 
-        nextPositions.right.x = me.position.xCur + distance;
-        nextPositions.right.y = me.position.yCur;
-        nextPositions.right.good = true;
+		for (var i = 0; i < selectedStrategy.length; i++) {
+			var pos = _.find(positions, {move: selectedStrategy[i]});
+			out.push(pos);
+		}
+		return out;
+	};
 
-        nextPositions.bottom.x = me.position.xCur;
-        nextPositions.bottom.y = me.position.yCur + distance;
-        nextPositions.bottom.good = true;
+	var damageForMove = function(xCur, yCur) {
+		var moves = [];
+		var positions = sortingPositions(xCur, yCur);
+		for (var i = 0; i < 4; i++) {
+			moves.push(
+				{
+					move: positions[i].move,
+					newPosition: positions[i],
+					damage: damageForStep(positions[i]),
+					canIMove: canIMove(positions[i])
+				}
+			)
+		}
+		return moves;
+	};
 
-        nextPositions.left.x = me.position.xCur - distance;
-        nextPositions.left.y = me.position.yCur;
-        nextPositions.left.good = true;
+	var player = {
+		getNextAction: function(info, self) {
+			_step++;
+			_info = info;
+			_self = self;
 
-        canIStayHere = true;
+			_enemy = _.find(_info, function(o) {
+				return o.type === 1 && o.id !== _self.id;
+			});
 
-        _.forEach(enemyShots, function (shot) {
-            if (nextPositions.top.good && hitTest(nextPositions.top.x, nextPositions.top.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)) {
-                nextPositions.top.good = false;
-            }
-            if (nextPositions.right.good && hitTest(nextPositions.right.x, nextPositions.right.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.width)) {
-                nextPositions.right.good = false;
-            }
-            if (nextPositions.bottom.good && hitTest(nextPositions.bottom.x, nextPositions.bottom.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)) {
-                nextPositions.bottom.good = false;
-            }
-            if (nextPositions.left.good && hitTest(nextPositions.left.x, nextPositions.left.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.width)) {
-                nextPositions.left.good = false;
-            }
+			var dfm = damageForMove(self.position.xCur, self.position.yCur);
 
+			dfm = _.chain(dfm).filter({canIMove: true}).map(function(m) {
+				m.aftermath = _.minBy(damageForMove(m.newPosition.x, m.newPosition.y), 'damage').damage + m.damage;
+				return m;
+			}).value();
+			if (_.filter(dfm, {damage: 0}).length === dfm.length) {
+				return core.actionTypes.fire;
+			}
 
-            if (canIStayHere) {
-                if (hitTest(me.position.xCur - distance, me.position.yCur - distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)
-                    || hitTest(me.position.xCur + distance, me.position.yCur - distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)
-                    || hitTest(me.position.xCur - distance, me.position.yCur + distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)
-                    || hitTest(me.position.xCur + distance, me.position.yCur + distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)
-                    || hitTest(nextPositions.top.x, nextPositions.top.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)
-                    || hitTest(nextPositions.right.x, nextPositions.right.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.width)
-                    || hitTest(nextPositions.bottom.x, nextPositions.bottom.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height)
-                    || hitTest(nextPositions.left.x, nextPositions.left.y, hitArea, shot.position.xCur, shot.position.yCur, shot.size.width)) {
-                    canIStayHere = false;
-                }
-            }
-        });
+			return _direction = _.minBy(dfm, 'aftermath').move;
 
-        if (me.position.yCur <= 0) {
-            nextPositions.top.good = false;
-        }
-        if (me.position.xCur >= 450) {
-            nextPositions.right.good = false;
-        }
-        if (me.position.yCur >= 450) {
-            nextPositions.bottom.good = false;
-        }
-        if (me.position.xCur <= 0) {
-            nextPositions.left.good = false;
-        }
+		},
+		name: 'Artik'
+	};
 
-    }
-
-    // var needToRun = false;
-    // var needToRunSteps = 0;
-    // var needToRunMove = 1;
-
-    var player = {
-        getNextAction: function (info, self) {
-            // if (needToRun) {
-            //     if (needToRunSteps) {
-            //         needToRunSteps--;
-            //         console.log('run')
-            //         return needToRunMove;
-            //     } else {
-            //         needToRun = false
-            //     }
-            // }
-
-            // var letsRun = false;
-            // var distance = 50;
-            // var hitArea = 50;
-
-            // var f = _.filter(info, function (o) {
-            //     return o.id != self.id;
-            // })
-            // _.forEach(f, function (shot) {
-            //     if (!letsRun) {
-            //         if (hitTest(self.position.xCur - distance, self.position.yCur - distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height) ||
-            //             hitTest(self.position.xCur + distance, self.position.yCur - distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height) ||
-            //             hitTest(self.position.xCur - distance, self.position.yCur + distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height) ||
-            //             hitTest(self.position.xCur + distance, self.position.yCur + distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height) ||
-            //             hitTest(self.position.xCur - distance, self.position.yCur, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height) ||
-            //             hitTest(self.position.xCur + distance, self.position.yCur, hitArea, shot.position.xCur, shot.position.yCur, shot.size.width) ||
-            //             hitTest(self.position.xCur, self.position.yCur - distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.height) ||
-            //             hitTest(self.position.xCur, self.position.yCur + distance, hitArea, shot.position.xCur, shot.position.yCur, shot.size.width)) {
-            //             letsRun = true;
-            //         }
-            //     }
-            // });
-            var damageForMove = [
-                {
-                    move: core.actionTypes.moveUp,
-                    damage: damageForStep(info, self, {
-                        x: self.position.xCur,
-                        y: self.position.yCur - 50
-                    })
-                },
-                {
-                    move: core.actionTypes.moveRight,
-                    damage: damageForStep(info, self, {
-                        x: self.position.xCur + 50,
-                        y: self.position.yCur
-                    })
-                },
-                {
-                    move: core.actionTypes.moveDown,
-                    damage: damageForStep(info, self, {
-                        x: self.position.xCur,
-                        y: self.position.yCur + 50
-                    })
-                },
-                {
-                    move: core.actionTypes.moveLeft,
-                    damage: damageForStep(info, self, {
-                        x: self.position.xCur - 50,
-                        y: self.position.yCur
-                    })
-                },
-            ];
-
-            var allMinimals = _.filter(damageForMove, { damage: 0 }).length === 4;
-            if (allMinimals) {
-                console.log('fire!')
-                return core.actionTypes.fire;
-            }
-
-            damageForMove = _.shuffle(damageForMove);
-            var min = _.minBy(damageForMove, 'damage');
-
-            console.warn('minimumDamage:', min.damage);
-            return min.move;
-
-            // return min.move;
-
-            // if (nextPositions.top.good && !nextPositions.bottom.good) {
-            //     return core.actionTypes.moveUp
-            // }
-            // if (nextPositions.right.good && !nextPositions.left.good) {
-            //     return core.actionTypes.moveRight
-            // }
-            // if (nextPositions.bottom.good && !nextPositions.top.good) {
-            //     return core.actionTypes.moveDown
-            // }
-            // if (nextPositions.left.good && !nextPositions.right.good) {
-            //     return core.actionTypes.moveLeft
-            // }
-
-
-
-            // var deltaTop = me.position.yCur,
-            //     deltaBottom = 450 - me.position.yCur,
-            //     deltaLeft = me.position.xCur,
-            //     deltaRight = 450 - me.position.xCur;
-
-            // if (nextPositions.top.good) {
-            //     return core.actionTypes.moveUp
-            // }
-            // if (nextPositions.bottom.good) {
-            //     return core.actionTypes.moveDown
-            // }
-            // if (nextPositions.left.good) {
-            //     return core.actionTypes.moveLeft
-            // }
-            // if (nextPositions.right.good) {
-            //     return core.actionTypes.moveRight
-            // }
-
-
-
-
-
-
-
-
-
-            // var assumption = 100;
-
-            // if (deltaTop > deltaBottom + assumption) {
-            //     return core.actionTypes.moveUp
-            // }
-            // if (deltaBottom > deltaTop + assumption) {
-            //     return core.actionTypes.moveDown
-            // }
-            // if (deltaLeft > deltaRight + assumption) {
-            //     return core.actionTypes.moveLeft
-            // }
-            // if (deltaRight > deltaLeft + assumption) {
-            //     return core.actionTypes.moveRight
-            // }
-
-
-            // if (me.position.xCur >= 450 && me.position.yCur <= 0) {
-            //     var factor = (me.position.xCur - enemy.position.xCur) - (me.position.yCur - enemy.position.yCur);
-            //     if (factor > 0) {
-            //         return core.actionTypes.moveDown;
-            //     } else {
-            //         return core.actionTypes.moveLeft;
-            //     }
-            // }
-            console.warn('UNEXCEPTED STEP')
-            return core.actionTypes.fire;
-
-        },
-        name: 'Artik'
-    };
-
-    return player;
+	return player;
 });
 
