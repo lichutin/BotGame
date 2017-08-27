6763e90c3e42f0a0c38acfb266912b974cd3d477
Artik Man <artik.alpha@gmail.com>
2017-08-27 18:03:53 +0300

    UPD: botArtik (bullets vector)

--- public_html/js/app/bots/botArtik.js
+++ public_html/js/app/bots/botArtik.js
@@ -4,12 +4,20 @@ define(['game/core', '_'], function(core, _) {
 		_info,
 		_self,
 		_enemy,
-		_direction = 1;
+		_direction = 1,
+		_enemyShots = [],
+		_shotSpeed = 25,
+		_shots = {};
 
 	// сделать возможность определения координаты пули на следующий ход
 
 	var hitTest = function(x1, y1, size1, x2, y2, size2) {
-		return ((x1 + size1 >= x2) && (x1 <= x2 + size2) && (y1 + size1 >= y2) && (y1 <= y2 + size2));
+		// return ((x1 + size1 >= x2) && (x1 <= x2 + size2) && (y1 + size1 >= y2) && (y1 <= y2 + size2));
+		var deltaX = x2 - x1;
+		var deltaY = y2 - y1;
+
+		return ((deltaX > -size2 && deltaX < size1) && (deltaY > -size2 && deltaY < size1))
+
 	};
 
 	var canIMove = function(nextStep) {
@@ -20,13 +28,15 @@ define(['game/core', '_'], function(core, _) {
 	};
 
 	var damageForStep = function(nextStep) {
-		var enemyShots = _.filter(_info, function(o) {
-			return o.type === 2 && o.pid === _enemy.id;
-		});
-		enemyShots.push(_enemy);
+		var x1 = nextStep.x,
+			y1 = nextStep.y,
+			size1 = _self.size.width;
 
-		return _.filter(enemyShots, function(shot) {
-			return (hitTest(nextStep.x, nextStep.y, _self.size.width, shot.position.xCur, shot.position.yCur, shot.size.height))
+		return _.filter(_enemyShots, function(shot) {
+			var x2 = _shots[shot.id].nextPosition.xCur,
+				y2 = _shots[shot.id].nextPosition.yCur,
+				size2 = shot.size.width;
+			return (hitTest(x1, y1, size1, x2, y2, size2))
 		}).length;
 	};
 
@@ -85,6 +95,80 @@ define(['game/core', '_'], function(core, _) {
 		return moves;
 	};
 
+	var distance = function(position, target) {
+		var deltaX = (target.xCur - position.xCur);
+		var deltaY = (target.yCur - position.yCur);
+		return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+	};
+
+	var nextBulletPosition = function(position, target, speed) {
+		var deltaX = (target.xCur - position.xCur);
+		var deltaY = (target.yCur - position.yCur);
+		var long = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+		var k = 0;
+		if (long !== 0) {
+			k = speed / long;
+		}
+		var stepX = k * deltaX;
+		var stepY = k * deltaY;
+
+		return {
+			xCur: position.xCur + stepX,
+			yCur: position.yCur + stepY
+		};
+	};
+
+	var cleanBullets = function() {
+		if (_step % 100 === 0) {
+			var currentBullets = _.map(_enemyShots, function(s) {
+				return s.id;
+			});
+
+			_.forIn(_shots, function(val, key) {
+				if (currentBullets.indexOf(parseInt(key)) === -1) {
+					delete _shots[key];
+				}
+			});
+		}
+	};
+
+	var calcBullets = function() {
+		cleanBullets();
+		_.forEach(_enemyShots, function(shot) {
+
+			var s = _shots[shot.id];
+			if (s) {
+				_shots[shot.id].currentPosition.xCur = shot.position.xCur;
+				_shots[shot.id].currentPosition.yCur = shot.position.yCur;
+				var nextPos = nextBulletPosition(s.currentPosition, s.targetPosition, _shotSpeed);
+				_shots[shot.id].nextPosition.xCur = nextPos.xCur;
+				_shots[shot.id].nextPosition.yCur = nextPos.yCur;
+			} else {
+				var next = nextBulletPosition(shot.position, shot.target, _shotSpeed);
+				var target = nextBulletPosition(shot.position, shot.target, 500);
+				_shots[shot.id] = {
+					initialPosition: {
+						xCur: shot.position.xCur,
+						yCur: shot.position.yCur
+					},
+					currentPosition: {
+						xCur: shot.position.xCur,
+						yCur: shot.position.yCur
+					},
+					nextPosition: {
+						xCur: next.xCur,
+						yCur: next.yCur
+					},
+					targetPosition: {
+						xCur: target.xCur,
+						yCur: target.yCur
+					}
+				}
+			}
+
+		});
+	};
+
 	var player = {
 		getNextAction: function(info, self) {
 			_step++;
@@ -95,7 +179,24 @@ define(['game/core', '_'], function(core, _) {
 				return o.type === 1 && o.id !== _self.id;
 			});
 
-			var dfm = damageForMove(self.position.xCur, self.position.yCur);
+			_enemyShots = _.filter(_info, function(o) {
+				return o.type === core.gameTypes.bullet && o.pid === _enemy.id;
+			});
+
+			calcBullets();
+
+			if (distance(_self.position, _enemy.position) > 50) {
+				var damageForStay = damageForStep({
+					x: _self.position.xCur,
+					y: _self.position.yCur
+				});
+				if (!damageForStay) {
+					return core.actionTypes.fire;
+				}
+			}
+
+
+			var dfm = damageForMove(_self.position.xCur, _self.position.yCur);
 
 			dfm = _.chain(dfm).filter({canIMove: true}).map(function(m) {
 				m.aftermath = _.minBy(damageForMove(m.newPosition.x, m.newPosition.y), 'damage').damage + m.damage;
